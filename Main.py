b87fd8c55b9636ca80cda5e6b6f426f22e223542
OliverPowell72 <55143548+OliverPowell72@users.noreply.github.com>
2020-11-16 15:58:47 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, session
  
 app = Flask(__name__)
 
@@ -14,13 +14,18 @@ def signup():
 def login():
     if request.method == "POST":
         user = request.form["nm"]
-        return redirect(url_for("user", usr=user))
+	session["user"] = user
+        return redirect(url_for("user"))
     else:
         return render_template("login.html")
 
-@app.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
+@app.route("/user")
+def user():
+	if "user" in session:
+		user = session["user"]
+    	return f"<h1>{user}</h1>"
+	else:
+		return redirecr(url_for("login.html"))
  
 if __name__ == "__main__":
     app.run(debug=True)
