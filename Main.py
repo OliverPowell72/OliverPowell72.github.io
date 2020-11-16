ba154a82fb262b1d242931aaed848870952d6ba7
OliverPowell72 <55143548+OliverPowell72@users.noreply.github.com>
2020-11-16 13:53:49 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,15 +2,15 @@ from flask import Flask, redirect, url_for, render_template, request
  
 app = Flask(__name__)
 
-@app.route("/", methods=["POST", "GET"])
+@app.route("/")
 def home():
 	if request.method == "POST":
 		if request.form.get("Signup"):
-			return redirect(url_for("/signup")
+			return redirect(url_for("signup")
 		elif request.form.get("Login"):
-			return redirect(url_for("/login")
-	elif request.method == "GET":
-			return render_template("index.html")
+			return redirect(url_for("login")
+	else:
+		return render_template("index.html")
 
 @app.route("/<user>")
 def user(usr):
