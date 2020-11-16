566bfacfae040d435396d99d6c855c46112aac5f
OliverPowell72 <55143548+OliverPowell72@users.noreply.github.com>
2020-11-16 12:32:56 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -13,7 +13,7 @@ def user(usr):
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-        user = request.form["USERNAME"]
+        user = request.form["nm"]
         return redirect(url_for("user", usr=user))
     else:
         return render_template("login.html")
