b5e7fd12dec1d11a51323590775338e227ea73b2
OliverPowell72 <55143548+OliverPowell72@users.noreply.github.com>
2020-11-17 17:59:15 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -14,6 +14,7 @@ users.append(User(id=1, username = "ollie", password = "password"))
 users.append(User(id=2, username = "ro", password = "secret"))
 
 app = Flask(__name__)
+app.secret_key = "hello"
 
 @app.route("/")
 def home():
