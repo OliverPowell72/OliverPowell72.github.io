05d7343345c9cbf57787905234f8fdc5d140a41e
OliverPowell72 <55143548+OliverPowell72@users.noreply.github.com>
2020-11-17 19:59:46 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,31 @@
 from flask import Flask, redirect, url_for, render_template, request, session, g
- 
+
 class User:
-	def __init__(self, id, username, password):
-		self.id = id
-		self.username = username
-		self.password = password
-	
-	def __repr__(self):
-		return f'<user: {self.username}>'
-	
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f'<User: {self.username}>'
+
 users = []
-users.append(User(id=1, username = 'ollie', password = 'password'))
-users.append(User(id=2, username = 'ro', password = 'secret'))
+users.append(User(id=1, username='ollie', password='password'))
+users.append(User(id=2, username='ro', password='sam'))
+users.append(User(id=3, username='tom', password='kirsch'))
+
 
 app = Flask(__name__)
-app.secret_key = 'akeythatshouldbesecret'
+app.secret_key = 'somesecretkeythatonlyishouldknow'
 
 @app.before_request
 def before_request():
-	g.user = None
-	
-	if 'user_id' in session:
-		user = [x for x in users if x.id == session['user_id']][0]
-		g.user = user
+    g.user = None
 
+    if 'user_id' in session:
+        user = [x for x in users if x.id == session['user_id']][0]
+        g.user = user
+        
 @app.route('/')
 def home():
 	return render_template('index.html')
@@ -34,24 +36,27 @@ def signup():
  
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	if request.method == 'POST':
-		session.pop('user_id', None)
-		
-		username = request.form['username']
-		password = request.form['password']
-		
-		user = [x for x in users if x.username == username][0]
-		if user and user.password == password:
-			session['user_id'] = user.id
-			return redirect(url_for('profile'))
-		
-		return redirect(url_for('login'))
-	
-	return render_template('login.html')
+    if request.method == 'POST':
+        session.pop('user_id', None)
+
+        username = request.form['username']
+        password = request.form['password']
+        
+        user = [x for x in users if x.username == username][0]
+        if user and user.password == password:
+            session['user_id'] = user.id
+            return redirect(url_for('profile'))
+
+        return redirect(url_for('login'))
+
+    return render_template('login.html')
 
 @app.route('/profile')
 def profile():
-	if not g.user:
-		return redirect(url_for('login'))
-	
-	return render_template('profile.html')
+    if not g.user:
+        return redirect(url_for('login'))
+
+    return render_template('profile.html')
+
+if __name__ == '__main__':
+	app.run(debug=True)
