62de8853d8f1977e0c52937f10bf68429facb273
OliverPowell72 <55143548+OliverPowell72@users.noreply.github.com>
2020-11-17 14:54:18 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,7 +4,16 @@ class User:
 	def __init__(self, id, username, password):
 		self.id = id
 		self.username = username
+		self.password = password
 	
+	def __repr__(self):
+		return f"<user: {self.username}>"
+	
+users = []
+users.append(User(id=1, username = "ollie", password = "password"))
+
+print(users)
+
 app = Flask(__name__)
 app.secret_key = "hello"
 
@@ -23,7 +32,6 @@ def login():
 @app.route("/profile")
 def user():
 	return render_template("profile.html")
-
  
 if __name__ == "__main__":
     app.run(debug=True)
